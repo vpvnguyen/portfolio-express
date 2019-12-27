7f4e46a482381e8119754df013da590f9e3665b9
vpvnguyen <vpvnguyen@gmail.com>
2019-12-27 14:24:05 -0800

    added json encoding and test GET api

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
+
+// handle url encoded data; parse json
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// make public static
+app.use(express.static('public'));
+
+// test get API
+app.get('/', (req, res) => {
+    res.send('/ GET');
+});
 
 app.listen(PORT, () => console.log(`Portfolio Express Server running on PORT: ${PORT}`));
