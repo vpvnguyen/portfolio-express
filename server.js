7a24bf086fc21ab43336f2e74bf7d028aceee4ba
vpvnguyen <vpvnguyen@gmail.com>
2020-01-11 15:35:36 -0800

    congifured dotenv, server-side logging, setup basic CRUD routes

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const fs = require('fs');
+const path = require('path');
+const morgan = require('morgan');
+
+// dot env
+require('dotenv').config();
+
+// connect to mongoDB
 const connectMongoDB = require('./config/mongoDB.js');
 
 // handle url encoded data; parse json
@@ -10,12 +18,34 @@ app.use(express.json());
 // make public static
 app.use(express.static('public'));
 
+// server side logging
+const  accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+app.use(morgan('combined', { accessLogStream }));
+
 // connect to mongoDB; ./config/mongoDB.js
 connectMongoDB();
 
-// test get API
+// GET - test
 app.get('/', (req, res) => {
     res.send('/ GET');
+});
+
+// POST - test
+app.post(`/api/:P_USERNAME`, (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
+
+// PUT - test
+app.put(`/api/:PUT`, (req, res) => {
+    console.log(req.body);
+    res.send('PUT');
+});
+
+// DEL - test
+app.delete(`/api/:DEL`, (req, res) => {
+    console.log(req.body);
+    res.send('DEL');
 });
 
 app.listen(PORT, () => console.log(`Portfolio Express Server running on PORT: ${PORT}`));
