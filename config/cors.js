4da5cb2d680630114d906e6921e68343051b623e
vpvnguyen <vpvnguyen@gmail.com>
2020-01-11 16:14:12 -0800

    modularized cors config

---
+++ config/cors.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+const corsOptions = {
+    origin: process.env.DEV_IP, // DEV_IP || PROD_IP - edit .env as well
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  };
+
+  module.exports = corsOptions;
