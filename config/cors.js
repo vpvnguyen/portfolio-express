3c8009a793ed5e05912d60f88f70597f78ef0d97
vpvnguyen <vpvnguyen@gmail.com>
2020-01-11 16:24:32 -0800

    fixed dev address

--- config/cors.js
+++ config/cors.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const corsOptions = {
-    origin: process.env.DEV_IP, // DEV_IP || PROD_IP - edit .env as well
+    origin: process.env.DEV_LOCALHOST, // DEV_IP || PROD_IP - edit .env as well
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   };
 
