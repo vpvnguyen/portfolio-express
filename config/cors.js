b563e56ccff037d825933360af08e5317c613b09
vpvnguyen <vpvnguyen@gmail.com>
2020-01-22 22:25:39 -0800

    defined options

--- config/cors.js
+++ config/cors.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 
+// configure cors for specific routes
 const corsOptions = {
-    origin: process.env.DEV_LOCALHOST, // DEV_IP || PROD_IP - edit .env as well
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    origin: 'http://localhost:3000/',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, // allow session cookie from browser to pass through
   };
 
   module.exports = corsOptions;
