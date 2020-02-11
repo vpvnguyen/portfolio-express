682332a07f46503f9c278c2e0a4b6bf94e4db5f7
Vincent Nguyen <vpvnguyen@gmail.com>
2020-02-10 23:24:37 -0800

    moved origin to dotenv

--- config/cors.js
+++ config/cors.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 // configure cors for specific routes
 const corsOptions = {
-    origin: 'http://localhost:3000/',
+    origin: process.env.CORS_ORIGIN,
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true, // allow session cookie from browser to pass through
